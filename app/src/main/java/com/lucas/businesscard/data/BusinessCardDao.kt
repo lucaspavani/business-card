e5ca2f0c183d449caa59dd89e813851dce177be7
lucaspavani <lucaspavanisantos@outlook.com>
2022-07-29 21:53:30 -0300

    Implementação da função share

--- app/src/main/java/com/lucas/businesscard/data/BusinessCardDao.kt
+++ app/src/main/java/com/lucas/businesscard/data/BusinessCardDao.kt
@@ -1,10 +1,7 @@
 package com.lucas.businesscard.data
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface BusinessCardDao {
@@ -13,5 +10,5 @@ interface BusinessCardDao {
     fun getAll(): LiveData<List<BusinessCard>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(businessCard: BusinessCard)
+    fun insert(businessCard: BusinessCard)
 }
--- app/src/main/java/com/lucas/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/lucas/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,11 +2,20 @@ package com.lucas.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.lucas.businesscard.App
+import com.lucas.businesscard.R
+import com.lucas.businesscard.data.BusinessCard
 import com.lucas.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +29,16 @@ class AddBusinessCardActivity : AppCompatActivity() {
         }
 
         binding.btnConfirm.setOnClickListener{
-
+            val businessCard = BusinessCard(
+                nome = binding.tilNome.editText?.text.toString(),
+                empresa = binding.tilNomeEmpresa.editText?.text.toString(),
+                telefone = binding.tilTelefone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                fundoPersonalizado = binding.tilCor.editText?.text.toString()
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_LONG).show()
+            finish()
         }
     }
 }
